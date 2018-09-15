70c25a75ad93e32995e05b38a6cc1d8867444acd
Eduardo Cueva <ecueva@penblu.com>
2018-09-15 11:34:11 -0500

    cambios de subidad de base se datos

--- db/installDatabases.sh
+++ db/installDatabases.sh
@@ -4,41 +4,51 @@ USER='uteg'
 PASS='Utegadmin2016*'
 CURRENT_DIR=`pwd`
 
+echo -n Escriba el password del Usuario Root de Mysql: 
+read -s ROOT_PASS
+echo $ROOT_PASS
+
 # CREACION DEL USUARIO MYSQL
-#mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-#mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
-#mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
-#mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+mysql -uroot -p${ROOT_PASS} -e "CREATE USER ${USER}@localhost IDENTIFIED BY '${PASS}';"
 
 # DATABASE ASGARD
-mysql -u$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/estructura/db_asgard.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/estructura/db_asgard.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_01.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_02.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_03.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_04.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_05.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_06.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_07.sql
+mysql -uroot -p${ROOT_PASS} -e "GRANT ALL PRIVILEGES ON db_asgard.* TO '${USER}'@'localhost';"
 
 # DATABASE GENERAL
-mysql -u$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/estructura/db_general.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_01.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_02.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_03.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_04.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_05.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_06.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_asgard_data_07.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/estructura/db_general.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_general_data.sql
+mysql -uroot -p${ROOT_PASS} -e "GRANT ALL PRIVILEGES ON db_general.* TO '${USER}'@'localhost';"
 
 # DATABASE CRM
-mysql -u$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/estructura/db_crm.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_crm_data.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/estructura/db_crm.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_crm_data.sql
+mysql -uroot -p${ROOT_PASS} -e "GRANT ALL PRIVILEGES ON db_crm.* TO '${USER}'@'localhost';"
 
 # DATABASE CAPTACION
-mysql -u$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/estructura/db_captacion.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_captacion_data_01.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_captacion_data_02.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/estructura/db_captacion.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_captacion_data_01.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_captacion_data_02.sql
+mysql -uroot -p${ROOT_PASS} -e "GRANT ALL PRIVILEGES ON db_captacion.* TO '${USER}'@'localhost';"
 
 # DATABASE ACADEMICO
-mysql -u$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/estructura/db_academico.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_academico_data_1.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_academico_data_2.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/estructura/db_academico.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_academico_data_1.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_academico_data_2.sql
+mysql -uroot -p${ROOT_PASS} -e "GRANT ALL PRIVILEGES ON db_academico.* TO '${USER}'@'localhost';"
 
 # DATABASE FACTURACION
-mysql -u$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/estructura/db_facturacion.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_facturacion_data_01.sql
-mysql -U$USER -p$PASS < $CURRENT_DIR/base_nueva_prod/data/db_facturacion_data_02.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/estructura/db_facturacion.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_facturacion_data_01.sql
+mysql -u${USER} -p${PASS} < $CURRENT_DIR/base_nueva_prod/data/db_facturacion_data_02.sql
+mysql -uroot -p${ROOT_PASS} -e "GRANT ALL PRIVILEGES ON db_facturacion.* TO '${USER}'@'localhost';"
+
+# FLUSH PRIVILEGES
+mysql -uroot -p${ROOT_PASS} -e "FLUSH PRIVILEGES;"
