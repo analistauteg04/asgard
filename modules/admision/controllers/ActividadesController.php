38ef0bdccfe06f3cb9b5258e4ce4c08be30be475
Eduardo Cueva <ecueva@penblu.com>
2018-09-16 08:44:37 -0500

    Se crean estructuras para actividades, interesados, oportunidades, solicitudes

--- modules/admision/controllers/ActividadesController.php
+++ modules/admision/controllers/ActividadesController.php
@@ -6,5 +6,33 @@ use Yii;
 
 class ActividadesController extends \app\components\CController
 {
+    public function actionIndex()
+    {
 
+    }
+
+    public function actionView()
+    {
+
+    }
+
+    public function actionEdit()
+    {
+
+    }
+
+    public function actionNew()
+    {
+        return $this->render('new');
+    }
+
+    public function actionSave()
+    {
+
+    }
+
+    public function actionUpdate()
+    {
+
+    }
 }
