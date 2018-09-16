a48ef20f25ea0078e5734106d636ce5e6e99df4b
Eduardo Cueva <ecueva@penblu.com>
2018-09-16 18:17:52 -0500

    Se suben cambios de actividad

--- modules/admision/controllers/ActividadesController.php
+++ modules/admision/controllers/ActividadesController.php
@@ -3,12 +3,25 @@
 namespace app\modules\admision\controllers;
 
 use Yii;
+use app\modules\admision\models\Oportunidad;
+use app\modules\admision\models\PersonaGestion;
 
 class ActividadesController extends \app\components\CController
 {
     public function actionIndex()
     {
-        
+        $modoportunidad = new Oportunidad();
+        $pges_id = base64_decode($_GET["pges_id"]);
+        $opor_id = base64_decode($_GET["opor_id"]);
+        $persges_mod = new PersonaGestion();
+        $contactManage = $persges_mod->consultarPersonaGestion($pges_id);
+        $mod_gestion = $modoportunidad->consultarOportunHist($opor_id);
+        $mod_oportu = $modoportunidad->consultarOportunidadById($opor_id);
+        return $this->render('index', [
+            'model' => $mod_gestion,
+            'personalData' => $contactManage,
+            'oportuniData' => $mod_oportu,
+        ]);
     }
 
     public function actionView()
--- modules/admision/views/actividades/index-grid.php
+++ modules/admision/views/actividades/index-grid.php
@@ -1,1 +1,61 @@
 <?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use app\widgets\PbGridView\PbGridView;
+use yii\data\ArrayDataProvider;
+
+//echo 'bbb '.base64_decode($_GET['codigo']);
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<?= Html::hiddenInput('txth_ids', $codigo, ['id' => 'txth_ids']); ?>
+
+<?=
+
+//   print_r($model);
+PbGridView::widget([
+    'id' => 'Pbgestion',
+    //'showExport' => true,
+    'fnExportEXCEL' => "exportExcel",
+    'fnExportPDF' => "exportPdf",
+    'dataProvider' => $model,
+    'columns' => [
+        [
+            'attribute' => 'fecha',
+            'header' => Yii::t("formulario", "Activity Date"),
+            'value' => 'fecha_atencion',
+        ],
+        [
+            'attribute' => 'descripcion',
+            'header' => Yii::t("formulario", "Description Activity"),
+            'contentOptions' => ['style' => 'max-width:200px;'],
+            'value' => 'observacion',
+        ],
+        [
+            'attribute' => 'fechaproxima',
+            'header' => Yii::t("formulario", "Attention Next"),
+            'value' => 'proxima_atencion',
+        ],
+        [
+            'attribute' => 'Estado',
+            'header' => Yii::t("crm", "Opportunity state"),
+            'value' => 'estado_oportunidad',
+        ],
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'header' => Yii::t("formulario", "Actions"),
+            'template' => '{view}', //
+            'buttons' => [
+                'view' => function ($url, $model) {
+                    return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', Url::to(['actividades/view', 'opid' => base64_encode($model["opo_id"]), 'pgid' => base64_encode($model["pges_id"]), 'acid' => base64_encode($model["bact_id"])]), ["data-toggle" => "tooltip", "title" => "Ver Actividad", "data-pjax" => 0]);
+                },
+            ],
+        ],
+    ],
+])
+?>
+
