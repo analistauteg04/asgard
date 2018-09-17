dd2d86037793e7c9434b9803b1598bc27a60d1e0
gkviterig <analistadesarrollo01@uteg.edu.ec>
2018-09-17 15:03:51 -0500

    Actualizaciones en proyecto asgard.

--- modules/admision/views/solicitudes/index.php
+++ modules/admision/views/solicitudes/index.php
@@ -1,1 +1,25 @@
 <?php
+
+use yii\helpers\Html;
+
+?>
+<?= Html::hiddenInput('txth_ids', '', ['id' => 'txth_ids']); ?>
+<div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+    <h3><span id="lbl_Personeria"><?= Yii::t("solicitud_ins", "Applications") ?></span></h3>
+</div>
+<div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+    <form class="form-horizontal">
+        <?=
+        $this->render('_formBuscarSolicitud', [
+        'arrCarreras' => $arrCarreras,
+        'arrEstados' => $arrEstados]);
+        ?>
+    </form>
+</div>
+<div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+    <?=
+        $this->render('index-grid', [
+        'model' => $model,
+        'url' => $url]);
+    ?>
+</div>
