be94a2927091a9e9f0bc59eaa80b987d596b9429
kleber Loayza <analistadesarrollo03.uteg.edu.ec>
2018-09-17 20:40:45 -0400

    se ha creado la vista de interesados en el nuevo modulo

--- modules/admision/views/interesados/index-grid.php
+++ modules/admision/views/interesados/index-grid.php
@@ -7,13 +7,10 @@ use app\widgets\PbGridView\PbGridView;
 <?=
 
 PbGridView::widget([
-    //'dataProvider' => new yii\data\ArrayDataProvider(array()),
     'id' => 'TbG_PERSONAS',
-    //'showExport' => true,
     'fnExportEXCEL' => "exportExcel",
     'fnExportPDF' => "exportPdf",
     'dataProvider' => $model,
-    //'pajax' => false,
     'columns' =>
     [
         [
@@ -34,11 +31,10 @@ PbGridView::widget([
         [
             'class' => 'yii\grid\ActionColumn',
             'header' => Yii::t("formulario", "Actions"),
-            //'headerOptions' => ['width' => '30'],
             'template' => '{solicitudes} ', //
             'buttons' => [
                 'solicitudes' => function ($url, $model) {
-                    return Html::a('<span class="glyphicon glyphicon-th-large"></span>', Url::to(['solicitudinscripcion/listarsolicitudxinteresado', 'ids' => base64_encode($model['per_id'])]), ["data-toggle" => "tooltip", "title" => "Mostrar Solicitudes", "data-pjax" => 0]);
+                    return Html::a('<span class="glyphicon glyphicon-th-large"></span>', Url::to(['admision/solicitud/solicitudxinteresado', 'ids' => base64_encode($model['id'])]), ["data-toggle" => "tooltip", "title" => "Mostrar Solicitudes", "data-pjax" => 0]);
                 },
             ],
         ],
