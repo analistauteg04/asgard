c6b27bd91cb73d6ee96d28921a1968062f3cbc27
GianVergara <analistadesarrollo02@uteg.edu.ec>
2018-09-20 12:35:07 -0500

    combos crear oportunidad modalidad

--- modules/admision/controllers/OportunidadesController.php
+++ modules/admision/controllers/OportunidadesController.php
@@ -156,7 +156,7 @@ class OportunidadesController extends \app\components\CController
             "tipo_dni" => array("CED" => Yii::t("formulario", "DNI Document"), "PASS" => Yii::t("formulario", "Passport")),
             "arr_carrerra2" => ArrayHelper::map($arr_carrerra2, "id", "name"),
             "arr_subcarrerra" => ArrayHelper::map($arr_subcarrera, "id", "name"),
-            'arr_empresa' => ArrayHelper::map($empresa, "Ids", "Nombre"),
+            'arr_empresa' => ArrayHelper::map($empresa, "id", "value"),
             'arr_moduloEstudio' => ArrayHelper::map($arr_moduloEstudio, "Ids", "Nombre"),
             'opo_id' => $opor_id,
             'pges_id' => $pges_id,
@@ -177,7 +177,7 @@ class OportunidadesController extends \app\components\CController
         $state_oportunidad_model = new EstadoOportunidad();
         //$academic_study = new EstudioAcademico();
         $unidad_acad_data = $uni_aca_model->consultarUnidadAcademicas();
-        $modalidad_data = $modalidad_model->consultarModalidad(0);
+        $modalidad_data = $modalidad_model->consultarModalidad(1);
         $modcanal = new Oportunidad();
         $tipo_oportunidad_data = $modTipoOportunidad->consultarOporxUnidad(1);
         $state_oportunidad_data = $state_oportunidad_model->consultarEstadOportunidad();
