8b7ef6b3484f6463255e0918adf456309f662944
GianVergara <analistadesarrollo02@uteg.edu.ec>
2018-09-26 09:03:16 -0500

    cambios varios en inscripciones, ulink y smart

--- modules/admision/models/Oportunidad.php
+++ modules/admision/models/Oportunidad.php
@@ -240,10 +240,13 @@ class Oportunidad extends \app\modules\admision\components\CActiveRecord {
                     concat(ifnull(agent.per_pri_nombre,''), ' ', ifnull(agent.per_pri_apellido,'')) as agente, 
                     -- uac.uaca_nombre as linea_servicio,
                     -- moda.mod_nombre as modalidad,
+                    (select uac.uaca_nombre from " . $con2->dbname . ".unidad_academica uac where uac.uaca_id = op.uaca_id) as linea_servicio,
+                    (select moda.mod_nombre from " . $con2->dbname . ".modalidad moda where moda.mod_id = op.mod_id) as modalidad,
                     pges.pges_cedula as identificacion,
                     eo.eopo_id as estado_oportunidad_id,
                     eo.eopo_nombre as estado_oportunidad,
-                    tov.tove_nombre as tipo_oportunidad -- ,
+                    tov.tove_nombre as tipo_oportunidad,
+                    (select eaca.eaca_nombre from " . $con2->dbname . ".estudio_academico eaca where eaca.eaca_id = op.eaca_id) as curso
                     -- case uac.uaca_id
                     --  when 1 then (select eaca.eaca_nombre from " . $con2->dbname . ".estudio_academico eaca where eaca.eaca_id = op.eaca_id)
                     --  when 2 then (select eaca.eaca_nombre from " . $con2->dbname . ".estudio_academico eaca where eaca.eaca_id = op.eaca_id)
