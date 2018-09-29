33001ebc43cebf8ad2257384d4560c0fecb9bd96
Developer <developer@uteg.edu.ec>
2018-09-29 18:54:19 -0500

    Inplmentaicon de Secuencial APPS

---
+++ modules/financiero/models/Secuencias.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use \yii\data\ActiveDataProvider;
+use \yii\data\ArrayDataProvider;
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Secuencias extends \yii\db\ActiveRecord {
+    /**
+     * @inheritdoc
+     */
+    public static function tableName() {
+        return 'secuencias';
+    }
+    
+    public static function nuevaSecuencia($con,$emp_id = NULL,$estab_id = NULL,$pemis_id = NULL,$secu_tipo_doc = NULL){
+        $numero=0;
+        //$sql = "SELECT ifnull(MAX(CAST(opo_codigo AS UNSIGNED)),0) AS id   ";          
+        //$sql="SELECT IFNULL(CAST(secuencia AS UNSIGNED),0) secuencia FROM " . $con->dbname . ".secuencias 
+        $sql="SELECT IFNULL(CAST(secuencia AS UNSIGNED),0) secuencia FROM db_facturacion.secuencias 
+                WHERE secu_estado=1  ";
+        if ($emp_id != NULL){$sql.=" AND emp_id=:emp_id ";}
+        if ($estab_id != NULL){$sql.=" AND estab_id=:estab_id ";}
+        if ($pemis_id != NULL){$sql.=" AND pemis_id=:pemis_id ";}
+        if ($secu_tipo_doc != NULL){$sql.=" AND secu_tipo_doc=:secu_tipo_doc ";}
+        $sql.=" FOR UPDATE ";
+       
+        $comando = $con->createCommand($sql);
+        $comando->bindParam(":emp_id", $emp_id, \PDO::PARAM_INT);
+        $comando->bindParam(":estab_id", $estab_id, \PDO::PARAM_INT);
+        $comando->bindParam(":pemis_id", $pemis_id, \PDO::PARAM_INT);
+        $comando->bindParam(":secu_tipo_doc", $secu_tipo_doc, \PDO::PARAM_STR);
+        //$rawData=$comando->queryAll();     
+        $rawData=$comando->queryScalar();   
+        //if (count($rawData) > 0) { 
+        if ($rawData !== false){
+            //$numero=str_pad((int)$rawData[0]["secuencia"]+1, 9, "0", STR_PAD_LEFT);
+            $numero=str_pad((int)$rawData + 1, 9, "0", STR_PAD_LEFT);
+            //$sql=" UPDATE " . $con->dbname . ".secuencias SET secuencia=:numero "
+            $sql=" UPDATE db_facturacion.secuencias SET secuencia=:secuencia "
+                    . " WHERE secu_estado=1 AND secu_tipo_doc=:secu_tipo_doc ";            
+            if ($emp_id != NULL){$sql.=" AND emp_id=:emp_id ";}
+            if ($estab_id != NULL){$sql.=" AND estab_id=:estab_id ";}
+            if ($pemis_id != NULL){$sql.=" AND pemis_id=:pemis_id ";}
+            
+            $comando = $con->createCommand($sql);
+            $comando->bindParam(":secuencia", $numero, \PDO::PARAM_STR);
+            $comando->bindParam(":emp_id", $emp_id, \PDO::PARAM_INT);
+            $comando->bindParam(":estab_id", $estab_id, \PDO::PARAM_INT);
+            $comando->bindParam(":pemis_id", $pemis_id, \PDO::PARAM_INT);
+            $comando->bindParam(":secu_tipo_doc", $secu_tipo_doc, \PDO::PARAM_STR);
+            $rawData=$comando->execute();         
+        }
+
+        return $numero;
+
+        
+    }
+    
+    
+    
+}
