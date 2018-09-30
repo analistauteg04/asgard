33015b1a8a7a058400c5322eb984c205dad1e258
kleber Loayza <analistadesarrollo03.uteg.edu.ec>
2018-09-30 17:24:37 -0400

    reporte pdf de admitidos

--- modules/academico/messages/es/Academico.php
+++ modules/academico/messages/es/Academico.php
@@ -7,14 +7,19 @@ namespace app\modules\academico\messages;
  */
  return [
      "Academic unit" => "Unidad Académica",
+     "Application date" => "Fecha Aplicacion",     
      "Aca. Uni." => "Uni. Aca.",
      "Attach document" => "Adjuntar Documentos",
      "Career/Program" => "Carrera/Programa",
      "Income Method" => "Mét. Ing",
      "Lecturing Period" => "Período Académico",
      "List Admitted" => "Listar Admitidos",
+     "# Solicitude" => "# Solicitud",     
+     "Admitted" => "Admitidos",
      "Modality" => "Modalidad",
      "Parallel" => "Paralelo",
      "Registered" => "Matriculado",
      "Registration by Homologation" => "Matriculación por Homologación",
  ];
+
+ 
--- modules/academico/views/admitidos/js/admitidos.js
+++ modules/academico/views/admitidos/js/admitidos.js
@@ -18,7 +18,7 @@ function exportPdf() {
     var f_ini = $('#txt_fecha_ini').val();
     var f_fin = $('#txt_fecha_fin').val();
     var carrera = $('#cmb_carrera option:selected').val();     
-    window.location.href = $('#txth_base').val() + "/academico/admitidos/exppdfadmitido?pdf=1&search=" + search + "&carrera=" + carrera+ "&fecha_ini=" + f_ini+ "&fecha_fin=" + f_fin;            
+    window.location.href = $('#txth_base').val() + "/academico/admitidos/exppdf?pdf=1&search=" + search + "&carrera=" + carrera+ "&fecha_ini=" + f_ini+ "&fecha_fin=" + f_fin;            
 }
 
 function actualizarGrid() {
