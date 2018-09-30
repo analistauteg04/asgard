e71209628912c0fb051069c2190f4b10331c7a26
Eduardo Cueva <ecueva@penblu.com>
2018-09-30 17:39:16 -0500

    Se sube files de aplicacion de cambios

--- db/configurarDirectorios.sh
+++ db/configurarDirectorios.sh
@@ -9,3 +9,15 @@ find $BASE_URL/asgard/ -type f -exec chmod 0644 {} \;
 chown -R apache:apache $BASE_URL/runtime
 chown -R apache:apache $BASE_URL/uploads
 chown -R apache:apache $BASE_URL/web/assets
+
+#BORRADO DE TODA CARPETA CON EXTENSION .GIT
+find $BASE_URL/asgard/ -name .gitignore -exec 'rm -rf {};'
+find $BASE_URL/asgard/ -name .git -exec 'rm -rf {};'
+
+#BORRADO DE ARCHIVO LOGS DEL PROYECTO
+rm -rf $BASE_URL/asgard/runtime/logs/*
+rm -rf $BASE_URL/asgard/nbproject
+rm -rf $BASE_URL/asgard/test
+
+#BORRADO DE ARCHIVO NO NECESARIOS DEL PROYECTO
+rm -rf $BASE_URL/asgard/*.md
