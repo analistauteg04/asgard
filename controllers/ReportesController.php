b9246476c341b443fb4d77f8e34ba11424935d1f
root <root@server.sea>
2018-10-08 17:38:40 -0500

    Reporte de Proxima Atencion

--- controllers/ReportesController.php
+++ controllers/ReportesController.php
@@ -25,22 +25,30 @@ class ReportesController extends CController {
     public function actionExpexcelreport(){
         $objDat= new Reporte();
         //$data["estado"]= $_GET["estado"];
-        $data["op"]= $_GET["op"];
+        $data["op"]= '2';//$_GET["op"];
         $data["f_ini"]= $_GET["f_ini"];
         $data["f_fin"]= $_GET["f_fin"];
         //$data["valor"]= $_GET["valor"];
-        $arrData=$objDat->consultarActividadporOportunidad($data);
+        
+        switch ($data["op"]) {
+            case '1'://GRADO
+                $arrData=$objDat->consultarActividadporOportunidad($data);
+                $arrHeader = array("N° Oport","Fecha","Nombres","Estado","Observacion","Detalle");
+                break;
+            case '2'://POSGRADO
+                $arrData=$objDat->consultarOportunidadProximaAten($data);
+                $arrHeader = array("N° Oport","F.Prox.At","Nombres","Estado","Observacion","Detalle");
+                break;
+            
+        }
 
         ini_set('memory_limit', '256M');
         $content_type = Utilities::mimeContentType("xls");
         $nombarch = "ActividadesOportunidad-" . date("YmdHis");
         header("Content-Type: $content_type");
         header("Content-Disposition: attachment;filename=" . $nombarch);
-        header('Cache-Control: max-age=0');
+        header('Cache-Control: max-age=0');       
         
-        //A.bact_id,B.opo_id,DATE(A.bact_fecha_registro) Fecha,CONCAT(C.pges_pri_nombre) Nombres,F.eopo_nombre,E.oact_nombre,A.bact_descripcion
-        //$arrHeader = array("#","Origen");
-        $arrHeader = array("Fecha","Nombres","Estado","Observacion","Detalle");
         $nameReport = yii::t("formulario", "Application Reports");
         $colPosition = array("C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
         Utilities::generarReporteXLS($nombarch, $nameReport, $arrHeader, $arrData, $colPosition);
--- models/Reporte.php
+++ models/Reporte.php
@@ -18,7 +18,7 @@ class Reporte extends \yii\db\ActiveRecord {
     
     public function consultarActividadporOportunidad($data) {
         $con = \Yii::$app->db_crm;  //A.bact_id,B.opo_id,      
-        $sql = "SELECT DATE(A.bact_fecha_registro) Fecha,CONCAT(C.pges_pri_nombre) Nombres,F.eopo_nombre,E.oact_nombre,A.bact_descripcion 
+        $sql = "SELECT LPAD(B.opo_id,9,'0') opo_id,DATE(A.bact_fecha_registro) Fecha,CONCAT(C.pges_pri_nombre) Nombres,F.eopo_nombre,E.oact_nombre,A.bact_descripcion 
                         FROM " . $con->dbname . ".bitacora_actividades A
                                 INNER JOIN (" . $con->dbname . ".oportunidad B
                                                 INNER JOIN " . $con->dbname . ".persona_gestion C
@@ -36,6 +36,26 @@ class Reporte extends \yii\db\ActiveRecord {
             $comando->bindParam(":f_ini",date("Y-m-d", strtotime($data['f_ini'])), \PDO::PARAM_STR);
             $comando->bindParam(":f_fin",date("Y-m-d", strtotime($data['f_fin'])), \PDO::PARAM_STR);
         }
+        return $comando->queryAll();
+    }
+    
+    public function consultarOportunidadProximaAten($data) {
+        $con = \Yii::$app->db_crm;       
+        $sql = "SELECT LPAD(B.opo_id,9,'0') opo_id,DATE(A.bact_fecha_proxima_atencion) F_Prox_At,CONCAT(C.pges_pri_nombre) Nombres,F.eopo_nombre,E.oact_nombre,A.bact_descripcion 
+                        FROM " . $con->dbname . ".bitacora_actividades A
+                                INNER JOIN (" . $con->dbname . ".oportunidad B
+                                                INNER JOIN " . $con->dbname . ".persona_gestion C
+                                                        ON B.pges_id=C.pges_id)
+                                        ON A.opo_id=B.opo_id
+                                INNER JOIN " . $con->dbname . ".observacion_actividades E						
+                                                        ON E.oact_id=A.oact_id
+                                INNER JOIN " . $con->dbname . ".estado_oportunidad F
+                                                        ON F.eopo_id=A.eopo_id
+                WHERE A.bact_estado=1  ";
+        $sql .= " AND DATE(A.bact_fecha_proxima_atencion) >= CURDATE() ";        
+        $sql .= " ORDER BY A.bact_fecha_proxima_atencion; "; 
+        $comando = $con->createCommand($sql);
+   
         return $comando->queryAll();
     }
     
