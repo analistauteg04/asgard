db84831858c6959ed0559bdfdfc8c0116fe11cb7
Developer <developer@uteg.edu.ec>
2018-10-26 17:16:59 -0500

    se ha creado la pantalla

--- modules/academico/views/matriculadosreprobados/_form_tab2.php
+++ modules/academico/views/matriculadosreprobados/_form_tab2.php
@@ -192,63 +192,6 @@ $leyenda = '<div class="col-md-12 col-xs-12 col-sm-12 col-lg-12">
             </div>
         </div>
     </div>
-    <div class="col-md-12 col-xs-12 col-sm-12 col-lg-12 doc_foto cinteres">
-        <div class="form-group">
-            <label for="txth_doc_foto" class="col-sm-3 col-md-3 col-xs-3 col-lg-3 control-label keyupmce"><?= Yii::t("formulario", "Foto") ?></label>
-            <div class="col-sm-9 col-md-9 col-xs-9 col-lg-9">
-                <?= Html::hiddenInput('txth_doc_foto', '', ['id' => 'txth_doc_foto']); ?>
-                <?php
-                echo CFileInputAjax::widget([
-                    'id' => 'txt_doc_foto',
-                    'name' => 'txt_doc_foto',
-                    'pluginLoading' => false,
-                    'showMessage' => false,
-                    'pluginOptions' => [
-                        'showPreview' => false,
-                        'showCaption' => true,
-                        'showRemove' => true,
-                        'showUpload' => false,
-                        'showCancel' => false,
-                        'browseClass' => 'btn btn-primary btn-block',
-                        'browseIcon' => '<i class="fa fa-folder-open"></i> ',
-                        'browseLabel' => "Subir Archivo",
-                        'uploadUrl' => Url::to(['/inscripcionadmision/saveinscripciontemp']),
-                        'maxFileSize' => Yii::$app->params["MaxFileSize"], // en Kbytes
-                        'uploadExtraData' => 'javascript:function (previewId,index) {
-            return {"upload_file": true, "name_file": "doc_foto", "inscripcion_id": $("#txth_twin_id").val()};
-        }',
-                    ],
-                    'pluginEvents' => [
-                        "filebatchselected" => "function (event) {
-        $('#txth_doc_foto').val($('#txt_doc_foto').val());
-        $('#txt_doc_foto').fileinput('upload');
-    }",
-                        "fileuploaderror" => "function (event, data, msg) {
-        $(this).parent().parent().children().first().addClass('hide');
-        $('#txth_doc_adj_disi').val('');
-        //showAlert('NO_OK', 'error', {'wtmessage': objLang.Error_to_process_File__Try_again_, 'title': objLang.Error});   
-    }",
-                        "filebatchuploadcomplete" => "function (event, files, extra) { 
-        $(this).parent().parent().children().first().addClass('hide');
-    }",
-                        "filebatchuploadsuccess" => "function (event, data, previewId, index) {
-        var form = data.form, files = data.files, extra = data.extra,
-        response = data.response, reader = data.reader;
-        $(this).parent().parent().children().first().addClass('hide');
-        var acciones = [{id: 'reloadpage', class: 'btn btn-primary', value: objLang.Accept, callback: 'reloadPage'}];
-        //showAlert('OK', 'Success', {'wtmessage': objLang.File_uploaded_successfully__Do_you_refresh_the_web_page_, 'title': objLang.Success, 'acciones': acciones});  
-    }",
-                        "fileuploaded" => "function (event, data, previewId, index) {
-        $(this).parent().parent().children().first().addClass('hide');
-        var acciones = [{id: 'reloadpage', class: 'btn btn-primary', value: objLang.Accept, callback: 'reloadPage'}];
-        //showAlert('OK', 'Success', {'wtmessage': objLang.File_uploaded_successfully__Do_you_refresh_the_web_page_, 'title': objLang.Success, 'acciones': acciones});                              
-    }",
-                    ],
-                ]);
-                ?>
-            </div>
-        </div>
-    </div>
     <div  id="divCertificado" style="display: none">   
         <div class="col-md-12 col-xs-12 col-sm-12 col-lg-12 doc_certificado cinteres">
             <div class="form-group">
@@ -401,7 +344,7 @@ $leyenda = '<div class="col-md-12 col-xs-12 col-sm-12 col-lg-12">
         </div>
         <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8"> &nbsp;</div>
         <div class="col-md-2">
-            <a id="sendInformacionAdmitidoPendDos" href="javascript:" class="btn btn-primary btn-block"> <?= Yii::t("formulario", "Next") ?> <span class="glyphicon glyphicon-menu-right"></span></a>
+            <a id="sendInformacionAdmitidoPendDos" href="javascript:" class="btn btn-primary btn-block"> <?= Yii::t("formulario", "Registrar") ?> <span class="glyphicon glyphicon-menu-right"></span></a>
         </div>
     </div>        
 </form>
