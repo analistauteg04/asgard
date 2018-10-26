54df3ba82dfc96b880d8c168937f36048caf85b0
GianVergara <analistadesarrollo02@uteg.edu.ec>
2018-10-26 11:29:25 -0500

    cambios nombre vista matricualdos reprobados

---
+++ modules/academico/views/matriculadosreprobados/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use yii\helpers\Html;
+use app\modules\academico\Module as academico;
+?>
+
+<div class="col-md-12">
+    <h3><span id="lbl_index"><?= 'Index Matriculados reprobados' ?></span></h3>
+</div>
