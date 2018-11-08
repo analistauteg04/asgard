1849465eb3c15c5e630a530520c745b466e3b025
Eduardo Cueva <ecueva@penblu.com>
2018-11-07 23:37:52 -0500

    Se actualiza cliente fe

--- modules/fe_edoc/clientWS/client-fe.php
+++ modules/fe_edoc/clientWS/client-fe.php
@@ -6,41 +6,4 @@ $obj = new SybaseFactura();
 //$res=$obj->consultarSybCabFacturas();
 $res = $obj->insertarFacturas();
 
-
-
-
-/* Web Server */
-$WS_HOST = "sistema.penblu.com";
-$WS_PORT = "80";
-$WS_USER = "test";
-$WS_PASS = "test";
-$WS_URI  = "asgard/api/request/fe_edoc/Client/getTest/json";
-$timeSend = 5; // en segundos
-
-$response = Http::connect($WS_HOST, $WS_PORT)->doPost($WS_URI, array('sleep' => 2));
-$arr_response = json_decode($response, true);
-
-putMessageLogFile($arr_response);
-
-function sendEdoc (){
-    GLOBAL $timeSend;
-    /* Proceso de lectura de datos */
-
-    sleep($timeSend);
-
-}
-
-function putMessageLogFile ($message) {
-    GLOBAL $logfile;
-    if (is_array($message))
-        $message = json_encode($message);
-    $message = date("Y-m-d H:i:s") . " " . $message . "\n";
-    if (!is_dir(dirname($logfile))) {
-        mkdir(dirname($logfile), 0777, true);
-        chmod(dirname($logfile), 0777);
-        touch($logfile);
-    }
-    file_put_contents($logfile, $message, FILE_APPEND | LOCK_EX);
-}
-
 ?>
