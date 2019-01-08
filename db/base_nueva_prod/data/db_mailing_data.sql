61635c202c5b29820f107fc81a7c7b7cb9510240
kleber Loayza <analistadesarrollo03.uteg.edu.ec>
2019-01-08 15:45:47 -0500

    se ha terminado la configuracion de base de datos para mailing

---
+++ db/base_nueva_prod/data/db_mailing_data.sql
@@ -0,0 +1,12 @@
+--
+-- Base de datos: `db_crm`
+--
+USE `db_mailing`;
+
+
+-- ---------------------------------------db_mailing---------------------------------------------------
+--
+-- Volcado de datos para la tabla `estado_cliente`
+--
+INSERT INTO `lista` (`lis_id`, `eaca_id`, `mest_id`, `lis_nombre`, `lis_descripcion`, `lis_estado`,`lis_fecha_creacion`, `lis_fecha_modificacion`, `lis_estado_logico`) VALUES
+(1, 2, null, 'Economia','Economia',1,'2019-08-10 01:06:52', NULL, '1');
