7104a0ee9ce324cfbd488dbfc33d572ec8ebd77f
GianVergara <analistadesarrollo02@uteg.edu.ec>
2019-01-08 15:44:44 -0500

    disaño de pantalla programacion en mailchaimp

--- modules/marketing/controllers/EmailController.php
+++ modules/marketing/controllers/EmailController.php
@@ -36,4 +36,19 @@ class EmailController extends \app\components\CController {
     public function actionIndex() {
         return $this->render('index', []);
     }
+
+    public function actionProgramacion() {
+        $per_id = @Yii::$app->session->get("PB_perid");
+        if (Yii::$app->request->isAjax) {
+            $data = Yii::$app->request->post();            
+            if (isset($data["gettemplate"])) {
+                //$template = $modlista->consultarListaTemplate($data["lista"]);
+                $message = array("template" => $template);
+                return Utilities::ajaxResponse('OK', 'alert', Yii::t('jslang', 'Success'), 'false', $message);
+            }
+        }
+        return $this->render('programacion', [
+        ]);
+    }
+
 }
