4114fe7b395e5e541bb348fd73bd656093803608
kleber Loayza <analistadesarrollo03.uteg.edu.ec>
2019-01-10 17:16:16 -0500

    valor

--- db/base_nueva_prod/data/db_mailing_data.sql
+++ db/base_nueva_prod/data/db_mailing_data.sql
@@ -11,7 +11,6 @@ USE `db_mailing`;
 INSERT INTO `lista` (`lis_id`, `eaca_id`, `mest_id`, `lis_nombre`, `lis_descripcion`, `lis_estado`,`lis_fecha_creacion`, `lis_fecha_modificacion`, `lis_estado_logico`) VALUES
 (1, 2, null, 'Economia','Economia',1,'2019-08-10 01:06:52', NULL, '1');
 
-
 -- 
 --
 -- Volcado de datos para la tabla `suscriptor`
