43354c7b265b9cf5bda8ca5420f125d8ceaa33a5
GianVergara <analistadesarrollo02@uteg.edu.ec>
2019-01-18 11:39:46 -0500

    funciones de editar y modificar programacion en mailing

--- modules/marketing/views/email/js/email.js
+++ modules/marketing/views/email/js/email.js
@@ -1,73 +1,73 @@
 $(document).ready(function () {
 
     // LUEGO BORRA CUANDO ESTE EL BOTON DE BD
-    $('#sendEditprograma').click(function () {
-        lista = $('#txth_list').val();
-        window.location.href = $('#txth_base').val() + "/marketing/email/editprogramacion?lisid=" + lista;
-    });
-    $('#sendModificaprograma').click(function () {
-        var link = $('#txth_base').val() + "/marketing/email/updateprogramacion";
-        var arrParams = new Object();
-        arrParams.check_dia_1 = "";
-        arrParams.check_dia_2 = "";
-        arrParams.check_dia_3 = "";
-        arrParams.check_dia_4 = "";
-        arrParams.check_dia_5 = "";
-        arrParams.check_dia_6 = "";
-        arrParams.check_dia_7 = "";
-        arrParams.lista = $('#txth_list').val();
-
-        if ($('input:checkbox[name=check_dia_1]:checked').val() > 0)
-        {
-            arrParams.check_dia_1 = $('input:checkbox[name=check_dia_1]:checked').val();
-        }
-        if ($('input:checkbox[name=check_dia_2]:checked').val() > 0)
-        {
-            arrParams.check_dia_2 = $('input:checkbox[name=check_dia_2]:checked').val();
-        }
-        if ($('input:checkbox[name=check_dia_3]:checked').val() > 0)
-        {
-            arrParams.check_dia_3 = $('input:checkbox[name=check_dia_3]:checked').val();
-        }
-        if ($('input:checkbox[name=check_dia_4]:checked').val() > 0)
-        {
-            arrParams.check_dia_4 = $('input:checkbox[name=check_dia_4]:checked').val();
-        }
-        if ($('input:checkbox[name=check_dia_5]:checked').val() > 0)
-        {
-            arrParams.check_dia_5 = $('input:checkbox[name=check_dia_5]:checked').val();
-        }
-        if ($('input:checkbox[name=check_dia_6]:checked').val() > 0)
-        {
-            arrParams.check_dia_6 = $('input:checkbox[name=check_dia_6]:checked').val();
-        }
-        if ($('input:checkbox[name=check_dia_7]:checked').val() > 0)
-        {
-            arrParams.check_dia_7 = $('input:checkbox[name=check_dia_7]:checked').val();
-        }
-        if (arrParams.check_dia_1 === "" && arrParams.check_dia_2 === "" && arrParams.check_dia_3 === "" && arrParams.check_dia_4 === "" && arrParams.check_dia_5 === "" && arrParams.check_dia_6 === "" && arrParams.check_dia_7 === "")
-        {
-            var mensaje =
-                    {wtmessage: "Días Programar : El campo no debe estar vacío.", title: "Error"};
-            showAlert("NO_OK", "error", mensaje);
-        } else {
-            arrParams.fecha_inicio = $('#txt_fecha_inicio').val();
-            arrParams.fecha_fin = $('#txt_fecha_fin').val();
-            arrParams.hora_envio = $('#txthoraenvio').val();
-            if (!validateForm()) {
-                requestHttpAjax(link, arrParams, function (response) {
-                    showAlert(response.status, response.label, response.message);
-                    if (!response.error) {
-                        setTimeout(function () {
-                            window.location.href = $('#txth_base').val() + "/marketing/email/index";
-                        }, 5000);
-                    }
-
-
-                }, true);
-            }
-        }
-    });
+    /*$('#sendEditprograma').click(function () {
+     lista = $('#txth_list').val();
+     window.location.href = $('#txth_base').val() + "/marketing/email/editprogramacion?lisid=" + lista;
+     });*/
+    /*$('#sendModificaprograma').click(function () {
+     var link = $('#txth_base').val() + "/marketing/email/updateprogramacion";
+     var arrParams = new Object();
+     arrParams.check_dia_1 = "";
+     arrParams.check_dia_2 = "";
+     arrParams.check_dia_3 = "";
+     arrParams.check_dia_4 = "";
+     arrParams.check_dia_5 = "";
+     arrParams.check_dia_6 = "";
+     arrParams.check_dia_7 = "";
+     arrParams.lista = $('#txth_list').val();
+     
+     if ($('input:checkbox[name=check_dia_1]:checked').val() > 0)
+     {
+     arrParams.check_dia_1 = $('input:checkbox[name=check_dia_1]:checked').val();
+     }
+     if ($('input:checkbox[name=check_dia_2]:checked').val() > 0)
+     {
+     arrParams.check_dia_2 = $('input:checkbox[name=check_dia_2]:checked').val();
+     }
+     if ($('input:checkbox[name=check_dia_3]:checked').val() > 0)
+     {
+     arrParams.check_dia_3 = $('input:checkbox[name=check_dia_3]:checked').val();
+     }
+     if ($('input:checkbox[name=check_dia_4]:checked').val() > 0)
+     {
+     arrParams.check_dia_4 = $('input:checkbox[name=check_dia_4]:checked').val();
+     }
+     if ($('input:checkbox[name=check_dia_5]:checked').val() > 0)
+     {
+     arrParams.check_dia_5 = $('input:checkbox[name=check_dia_5]:checked').val();
+     }
+     if ($('input:checkbox[name=check_dia_6]:checked').val() > 0)
+     {
+     arrParams.check_dia_6 = $('input:checkbox[name=check_dia_6]:checked').val();
+     }
+     if ($('input:checkbox[name=check_dia_7]:checked').val() > 0)
+     {
+     arrParams.check_dia_7 = $('input:checkbox[name=check_dia_7]:checked').val();
+     }
+     if (arrParams.check_dia_1 === "" && arrParams.check_dia_2 === "" && arrParams.check_dia_3 === "" && arrParams.check_dia_4 === "" && arrParams.check_dia_5 === "" && arrParams.check_dia_6 === "" && arrParams.check_dia_7 === "")
+     {
+     var mensaje =
+     {wtmessage: "Días Programar : El campo no debe estar vacío.", title: "Error"};
+     showAlert("NO_OK", "error", mensaje);
+     } else {
+     arrParams.fecha_inicio = $('#txt_fecha_inicio').val();
+     arrParams.fecha_fin = $('#txt_fecha_fin').val();
+     arrParams.hora_envio = $('#txthoraenvio').val();
+     if (!validateForm()) {
+     requestHttpAjax(link, arrParams, function (response) {
+     showAlert(response.status, response.label, response.message);
+     if (!response.error) {
+     setTimeout(function () {
+     window.location.href = $('#txth_base').val() + "/marketing/email/index";
+     }, 5000);
+     }
+     
+     
+     }, true);
+     }
+     }
+     });*/
 
     $('#cmb_lista').change(function () {
         var link = $('#txth_base').val() + "/marketing/email/programacion";
@@ -97,25 +97,25 @@ $(document).ready(function () {
                 data = response.message;
                 setComboDataselect(data.carrera, "cmb_carrera_programa", "Seleccionar");
             }
-        }, true);        
+        }, true);
         arrParams.emp_id = $(this).val();
         arrParams.getempresa = true;
-        requestHttpAjax(link, arrParams, function (response) {              
+        requestHttpAjax(link, arrParams, function (response) {
             if (response.status == "OK") {
                 data = response.message;
-                alert('empresa:'+arrParams.emp_id);
-                alert('sin pais');                
-                alert('pais:'+data.empresa.emp_telefono);
-               
+                alert('empresa:' + arrParams.emp_id);
+                alert('sin pais');
+                alert('pais:' + data.empresa.emp_telefono);
+
                 $('#cmb_pais').val(data.empresa.pai_id);
                 $('#cmb_provincia').val(data.empresa.pro_id);
                 $('#cmb_ciudad').val(data.empresa.can_id);
                 $('#txt_direccion1').val(data.empresa.emp_direccion);
                 $('#txt_direccion2').val(data.empresa.emp_direccion1);
                 $('#txt_telefono').val(data.empresa.emp_telefono);
-                $('#txt_codigo_postal').val(data.empresa.emp_codigo_postal);                                
+                $('#txt_codigo_postal').val(data.empresa.emp_codigo_postal);
             }
-        }, true);        
+        }, true);
     });
 
     $('#cmb_pais').change(function () {
@@ -372,6 +372,74 @@ function eliminarLista(id, codigo) {
     objAccept.paramCallback = params;
     messagePB.acciones = new Array();
     messagePB.acciones[0] = objAccept;
+
     showAlert("warning", "warning", messagePB);
+}
+function editarProgramacion() {
+    lista = $('#txth_list').val();
+    window.location.href = $('#txth_base').val() + "/marketing/email/editprogramacion?lisid=" + lista;
+}
+function modificarProgramacion() {
+    var link = $('#txth_base').val() + "/marketing/email/updateprogramacion";
+    var arrParams = new Object();
+    arrParams.check_dia_1 = "";
+    arrParams.check_dia_2 = "";
+    arrParams.check_dia_3 = "";
+    arrParams.check_dia_4 = "";
+    arrParams.check_dia_5 = "";
+    arrParams.check_dia_6 = "";
+    arrParams.check_dia_7 = "";
+    arrParams.lista = $('#txth_list').val();
+
+    if ($('input:checkbox[name=check_dia_1]:checked').val() > 0)
+    {
+        arrParams.check_dia_1 = $('input:checkbox[name=check_dia_1]:checked').val();
+    }
+    if ($('input:checkbox[name=check_dia_2]:checked').val() > 0)
+    {
+        arrParams.check_dia_2 = $('input:checkbox[name=check_dia_2]:checked').val();
+    }
+    if ($('input:checkbox[name=check_dia_3]:checked').val() > 0)
+    {
+        arrParams.check_dia_3 = $('input:checkbox[name=check_dia_3]:checked').val();
+    }
+    if ($('input:checkbox[name=check_dia_4]:checked').val() > 0)
+    {
+        arrParams.check_dia_4 = $('input:checkbox[name=check_dia_4]:checked').val();
+    }
+    if ($('input:checkbox[name=check_dia_5]:checked').val() > 0)
+    {
+        arrParams.check_dia_5 = $('input:checkbox[name=check_dia_5]:checked').val();
+    }
+    if ($('input:checkbox[name=check_dia_6]:checked').val() > 0)
+    {
+        arrParams.check_dia_6 = $('input:checkbox[name=check_dia_6]:checked').val();
+    }
+    if ($('input:checkbox[name=check_dia_7]:checked').val() > 0)
+    {
+        arrParams.check_dia_7 = $('input:checkbox[name=check_dia_7]:checked').val();
+    }
+    if (arrParams.check_dia_1 === "" && arrParams.check_dia_2 === "" && arrParams.check_dia_3 === "" && arrParams.check_dia_4 === "" && arrParams.check_dia_5 === "" && arrParams.check_dia_6 === "" && arrParams.check_dia_7 === "")
+    {
+        var mensaje =
+                {wtmessage: "Días Programar : El campo no debe estar vacío.", title: "Error"};
+        showAlert("NO_OK", "error", mensaje);
+    } else {
+        arrParams.fecha_inicio = $('#txt_fecha_inicio').val();
+        arrParams.fecha_fin = $('#txt_fecha_fin').val();
+        arrParams.hora_envio = $('#txthoraenvio').val();
+        if (!validateForm()) {
+            requestHttpAjax(link, arrParams, function (response) {
+                showAlert(response.status, response.label, response.message);
+                if (!response.error) {
+                    setTimeout(function () {
+                        window.location.href = $('#txth_base').val() + "/marketing/email/index";
+                    }, 5000);
+                }
+
+
+            }, true);
+        }
+    }
 }
 
