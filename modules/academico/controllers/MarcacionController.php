f98f77d74cecc5e6998f9128fa94a2a44ec5fc79
gkviterig <analistadesarrollo01@uteg.edu.ec>
2019-02-08 13:24:26 -0500

    Estructura de módulo marcación.

---
+++ modules/academico/controllers/MarcacionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\modules\academico\controllers;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use app\models\Utilities;
+
+
+class MarcacionController extends \app\components\CController {
+    
+}
