0b368c5753d5657a632a34536994f2e84125613f
GianVergara <analistadesarrollo02@uteg.edu.ec>
2019-02-25 18:23:02 -0500

    depurar codigo

--- modules/academico/controllers/MarcacionController.php
+++ modules/academico/controllers/MarcacionController.php
@@ -19,15 +19,14 @@ class MarcacionController extends \app\components\CController {
     public function actionMarcacion() {
         $mod_marcacion = new RegistroMarcacion();
         $per_id = @Yii::$app->session->get("PB_perid");
-        $dia = date("w", strtotime(date("Y-m-d")));       
-        $fecha_consulta = '';      
+        $dia = date("w", strtotime(date("Y-m-d")));
+        $fecha_consulta = '';
         $fecha_compara = date(Yii::$app->params["dateByDefault"]);
         $cons_distancia = $mod_marcacion->consultarFechaDistancia($fecha_compara, $per_id);
         // si valor devuelve 1 existe y fecha consulta toma el valor de fecha compra
         if ($cons_distancia["existe_distancia"] > 0) {
             $fecha_consulta = $fecha_compara;
-        }   
-        \app\models\Utilities::putMessageLogFile('si tiene: ' . $cons_distancia["existe_distancia"]);
+        }
         $arr_materia = $mod_marcacion->consultarMateriasMarcabyPro($per_id, $dia, $fecha_consulta);
         return $this->render('marcacion', [
                     'model' => $arr_materia
