41d8f4161f4908fe36fccc29d288f53903c45d61
root <root@server.sea>
2019-03-14 07:00:09 -0500

    funcion corrige caracteres

--- modules/fe_edoc/clientWS/libs/Global.php
+++ modules/fe_edoc/clientWS/libs/Global.php
@@ -15,7 +15,7 @@ $WS_URI = "asgard/api/request/fe_edoc/Edoc/sendEdoc/json";
 $service = "uteg-fe";
 $logFile = "logs/$service.log";
 $limit = 10;
-$timeWait=1;//1 Segundo
+$timeWait = 1; //1 Segundo
 
 function putMessageLogFile($message) {
     GLOBAL $logFile;
@@ -23,4 +23,22 @@ function putMessageLogFile($message) {
         $message = json_encode($message);
     $message = date("Y-m-d H:i:s") . " " . $message . "\n";
     file_put_contents($logFile, $message, FILE_APPEND | LOCK_EX);
+}
+
+function limpiarUTF8($cadena) {
+    $String = trim($cadena);
+    $String = str_replace("\u00e1", "á", $String);
+    $String = str_replace("\u00c1", "Á", $String);
+    $String = str_replace("\u00e9", "é", $String);
+    $String = str_replace("\u00c9", "É", $String);
+    $String = str_replace("\u00ed", "í", $String);
+    $String = str_replace("\u00cd", "Í", $String);
+    $String = str_replace("\u00f3", "ó", $String);
+    $String = str_replace("\u00d3", "Ó", $String);
+    $String = str_replace("\u00fa", "ú", $String);
+    $String = str_replace("\u00da", "Ú", $String);
+    $String = str_replace("\u00f1", "ñ", $String);
+    $String = str_replace("\u00d1", "Ñ", $String);
+
+    return $String;
 }
