9f8e835c0f9d686239368e8d4750b1b5fe0a541b
gkviterig <analistadesarrollo01@uteg.edu.ec>
2019-03-23 11:49:32 -0500

    Actualizaciones varias en carga de actividades en menú oportunidades.

--- modules/admision/models/BitacoraActividadesTmp.php
+++ modules/admision/models/BitacoraActividadesTmp.php
@@ -83,8 +83,7 @@ class BitacoraActividadesTmp extends \yii\db\ActiveRecord
         if (strtolower(end($chk_ext)) == "xls" || strtolower(end($chk_ext)) == "xlsx") {            
             //Create new PHPExcel object
             $objPHPExcel = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
-            $dataArr = array();
-            $model = new BitacoraActividadesTmp(); //isset       
+            $dataArr = array();            
             try {                
                 foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
                     $worksheetTitle = $worksheet->getTitle();
@@ -102,29 +101,18 @@ class BitacoraActividadesTmp extends \yii\db\ActiveRecord
                     unset($dataArr[1]); // Se elimina la cabecera de titulos del file
                 }
                 $this->deletetablaTemp($con, $usu_id);                
-                $filaError = 1;                    
+                $fila = 1;                    
                 foreach ($dataArr as $val) {                                        
-                    $filaError++;        
+                    $fila++;        
                     $modelPerGestion = new PersonaGestion();
                     $modelUnidad = new UnidadAcademica();
                     $modelModalidad = new Modalidad();
                     $modelEstAcademico = new EstudioAcademico();                                           
-                    $respIdPerGes = $modelPerGestion::existePersonaGestLeads($val[6], $val[5]);   
-                    if (!($respIdPerGes)) {
-                        $mensaje = "No se encuentra persona gestión creada.";                                                       
-                    }
-                    $respUnidad = $modelUnidad::consultarIdsUnid_Academica($val[1]);
-                    if (!($respUnidad)) {
-                        $mensaje = "No se encuentra unidad.";                        
-                    }
-                    $respModalidad = $modelModalidad::consultarIdsModalidad($val[2]);
-                    if (!($respModalidad)) {
-                        $mensaje = "No se encuentra modalidad.";                        
-                    }
-                    $respEstAcade = $modelEstAcademico::consultarIdsEstudioAca($val[3]);  
-                    if (!($respEstAcade)) {
-                        $mensaje = "No se encuentra carrera.";                        
-                    }                                        
+                    $model = new BitacoraActividadesTmp();
+                    $respIdPerGes = $modelPerGestion::existePersonaGestLeads($val[6], $val[5]);                       
+                    $respUnidad = $modelUnidad::consultarIdsUnid_Academica($val[1]);                    
+                    $respModalidad = $modelModalidad::consultarIdsModalidad($val[2]);                    
+                    $respEstAcade = $modelEstAcademico::consultarIdsEstudioAca($val[3]);                                                        
                     $respOport = $model->consultarOportunidad($emp_id, $respIdPerGes, $respEstAcade, $respUnidad, $respModalidad);                    
                     if (!($respOport)) {                           
                         $bandera= '0';
@@ -150,11 +138,10 @@ class BitacoraActividadesTmp extends \yii\db\ActiveRecord
                     if ($bandera == '0') {
                         $arroout["status"] = FALSE;
                         $arroout["error"] = null;
-                        $arroout["message"] = " Error en la Fila => N°$filaError Nombres => $val[4]";
+                        $arroout["message"] = " Error en la Fila => N°$fila Nombres => $val[4]";
                         $arroout["data"] = null;
-                        throw new Exception('Error, Item no almacenado');
-                    }
-                    \app\models\Utilities::putMessageLogFile('oport:'.$respOport["opo_id"]);
+                        throw new Exception('Error en la Fila => N°'.$fila. ' Nombres => '. $val[4]);
+                    }                    
                     $model->opo_id = $respOport["opo_id"];
                     $model->usu_id = $usu_id;
                     $model->padm_id = $padm_id;
@@ -168,17 +155,16 @@ class BitacoraActividadesTmp extends \yii\db\ActiveRecord
                         $model->oper_id =$val[12];                        
                     }
                     $model->bact_descripcion = $val[11];                                         
-                    if (!$model->save()) {     
-                        //\app\models\Utilities::putMessageLogFile('opor error:'.$respOport["opo_id"]);
+                    $model->save();                    
+                    if (empty($model->bact_id)) {                             
                         $arroout["status"] = FALSE;
                         $arroout["error"] = null;
-                        $arroout["message"] = " Error en la Fila => N°$filaError Nombres => $val[4]";
+                        $arroout["message"] = " Error en la Fila => N°$fila Nombres => $val[4]";
                         $arroout["data"] = null;
-                        throw new Exception('Error, Item no almacenado');
+                        throw new Exception('Error, al grabar actividad.');
                     }
                 } 
-                if ($trans !== null) 
-                    \app\models\Utilities::putMessageLogFile('graba');
+                if ($trans !== null)                    
                     $trans->commit(); 
                
                 $arroout["status"] = TRUE;
@@ -188,12 +174,13 @@ class BitacoraActividadesTmp extends \yii\db\ActiveRecord
                 return $arroout;
             } catch (Exception $ex) {
                 if ($trans !== null)                    
-                    $trans->rollback(); 
-                                    \app\models\Utilities::putMessageLogFile('rollbacjk');
+                    $trans->rollback();    
+                    \app\models\Utilities::putMessageLogFile('rollback');
                     $arroout["status"] = FALSE;
                     $arroout["error"] = null;
-                    $arroout["message"] = " Error en la Fila => N°$filaError Nombres => $val[4]. $mensaje";
+                    $arroout["message"] = " Error en la Fila => N°$fila Nombres => $val[4]. $mensaje";
                     $arroout["data"] = null;
+                    
                 return $arroout;
             }
         }
@@ -277,8 +264,7 @@ class BitacoraActividadesTmp extends \yii\db\ActiveRecord
         $comando->bindParam(":usu_id", $usu_id, \PDO::PARAM_INT);
         return $comando->queryAll();
     }
-    
-    
+        
     public function consultarOporPerdida($opoper_id) {
         $con = \Yii::$app->db_crm;
         $estado = 1;
@@ -291,149 +277,6 @@ class BitacoraActividadesTmp extends \yii\db\ActiveRecord
         $comando->bindParam(":oper_id", $opoper_id, \PDO::PARAM_INT);
         $resultData = $comando->queryOne();
         return $resultData;
-    }
-    
-     /**
-     * Function insertarBitacoraNovedades grabar novedades de actividades de una oportunidad.
-     * @author Grace Viteri <analistadesarrollo01@uteg.edu.ec>;
-     * @param
-     * @return
-     */
-    public function insertarBitacoraNovedades($bano_unidad, /*$bano_modalidad, $bano_carrera, $bano_nombre, $bano_telefono, 
-                                              $bano_correo, $bano_contacto, $eopo_id, $oact_id, $bano_fecha_registro, $bano_descripcion,
-                                              $bano_fecha_proxima_atencion, $oper_id, */$usu_id, $bano_tipoarchivo, $bano_novedad) {            
-        $con = \Yii::$app->db_crm;
-        $trans = $con->getTransaction(); // se obtiene la transacción actual
-        if ($trans !== null) {
-            $trans = null; // si existe la transacción entonces no se crea una
-        } else {
-            $trans = $con->beginTransaction(); // si no existe la transacción entonces se crea una
-        }       
-        if (isset($bano_unidad)) {
-            $param_sql .= ", bano_unidad";
-            $bdet_sql .= ", :bano_unidad";
-        }
-        /*if (isset($bano_modalidad)) {
-            $param_sql .= ", bano_modalidad";
-            $bdet_sql .= ", :bano_modalidad";
-        }
-        if (isset($bano_carrera)) {
-            $param_sql .= ", bano_carrera";
-            $bdet_sql .= ", :bano_carrera";
-        }
-        if (isset($bano_nombre)) {
-            $param_sql .= ", bano_nombre";
-            $bdet_sql .= ", :bano_nombre";
-        }
-        if (isset($bano_telefono)) {
-            $param_sql .= ", bano_telefono";
-            $bdet_sql .= ", :bano_telefono";
-        }
-        if (isset($bano_correo)) {
-            $param_sql .= ", bano_correo";
-            $bdet_sql .= ", :bano_correo";
-        }
-        if (isset($bano_contacto)) {
-            $param_sql .= ", bano_contacto";
-            $bdet_sql .= ", :bano_contacto";
-        }
-        if (isset($eopo_id)) {
-            $param_sql .= ", eopo_id";
-            $bdet_sql .= ", :eopo_id";
-        }
-        if (isset($oact_id)) {
-            $param_sql .= ", oact_id";
-            $bdet_sql .= ", :oact_id";
-        }
-        if (isset($bano_fecha_registro)) {
-            $param_sql .= ", bano_fecha_registro";
-            $bdet_sql .= ", :bano_fecha_registro";
-        }
-        if (isset($bano_descripcion)) {
-            $param_sql .= ", bano_descripcion";
-            $bdet_sql .= ", :bano_descripcion";
-        }
-        if (isset($bano_fecha_proxima_atencion)) {
-            $param_sql .= ", bano_fecha_proxima_atencion";
-            $bdet_sql .= ", :bano_fecha_proxima_atencion";
-        }
-        if (isset($oper_id)) {
-            $param_sql .= ", oper_id";
-            $bdet_sql .= ", :oper_id";
-        }*/
-        if (isset($usu_id)) {
-            $param_sql .= ", usu_id";
-            $bdet_sql .= ", :usu_id";
-        }
-        if (isset($bano_tipoarchivo)) {
-            $param_sql .= ", bano_tipoarchivo";
-            $bdet_sql .= ", :bano_tipoarchivo";
-        }
-        if (isset($bano_novedad)) {
-            $param_sql .= ", bano_novedad";
-            $bdet_sql .= ", :bano_novedad";
-        }
-        try {
-            $sql = "INSERT INTO " . $con->dbname . ".bitacora_actividades_noprocesadas ($param_sql) VALUES($bdet_sql)";
-            $comando = $con->createCommand($sql);
-
-            if (isset($bano_unidad)) {
-                $comando->bindParam(':bano_unidad', $bano_unidad, \PDO::PARAM_STR);
-            }
-           /* if (!empty((isset($bano_modalidad)))) {
-                $comando->bindParam(':bano_modalidad', $bano_modalidad, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($bano_carrera)))) {
-                $comando->bindParam(':bano_carrera', $bano_carrera, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($bano_nombre)))) {
-                $comando->bindParam(':bano_nombre', $bano_nombre, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($bano_telefono)))) {
-                $comando->bindParam(':bano_telefono', $bano_telefono, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($bano_correo)))) {
-                $comando->bindParam(':bano_correo', $bano_correo, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($bano_contacto)))) {
-                $comando->bindParam(':bano_contacto', $bano_contacto, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($eopo_id)))) {
-                $comando->bindParam(':eopo_id', $eopo_id, \PDO::PARAM_INT);
-            }
-            if (!empty((isset($oact_id)))) {
-                $comando->bindParam(':oact_id', $oact_id, \PDO::PARAM_INT);
-            }
-            if (!empty((isset($bano_fecha_registro)))) {
-                $comando->bindParam(':bano_fecha_registro', $bano_fecha_registro, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($bano_descripcion)))) {
-                $comando->bindParam(':bano_descripcion', $bano_descripcion, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($bano_fecha_proxima_atencion)))) {
-                $comando->bindParam(':bano_fecha_proxima_atencion', $bano_fecha_proxima_atencion, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($oper_id)))) {
-                $comando->bindParam(':oper_id', $oper_id, \PDO::PARAM_INT);
-            }*/
-            if (!empty((isset($usu_id)))) {
-                $comando->bindParam(':usu_id', $usu_id, \PDO::PARAM_INT);
-            }
-            if (!empty((isset($bano_tipoarchivo)))) {
-                $comando->bindParam(':bano_tipoarchivo', $bano_tipoarchivo, \PDO::PARAM_STR);
-            }
-            if (!empty((isset($bano_novedad)))) {
-                $comando->bindParam(':bano_novedad', $bano_novedad, \PDO::PARAM_STR);
-            }
-            $result = $comando->execute();
-            if ($trans !== null)
-                $trans->commit();
-            return $con->getLastInsertID($con->dbname . '.bitacora_actividades_noprocesadas');
-        } catch (Exception $ex) {
-            if ($trans !== null)
-                $trans->rollback();
-            return FALSE;
-        }
-    }
+    }    
 }
 
