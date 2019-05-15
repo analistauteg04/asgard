0245821401239753f6888933ca69c20dff119e11
kloayza23 <kleberloayzacastro@gmail.com>
2019-05-15 18:04:18 -0500

    actualizacion del archivo firewall

--- db/firewall.sh
+++ db/firewall.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
-
-IP_UTEG_DES="22.33.44.55"
+IP_UTEG_DES="181.39.139.67"
 IP_UTEG_OFI="23.34.44.55"
 echo "Aplicando Reglas de Firewall..."
 
 ## FLUSH de reglas
-iptables −F
-iptables −X
-iptables −Z
-iptables −t nat −F
+iptables -F
+iptables -X
+iptables -Z
+iptables -t nat -F
 
 ## Establecemos politica por defecto
-iptables −P INPUT DROP
-iptables −P OUTPUT DROP
-iptables −P FORWARD DROP
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
 
 ## Se permite todo para localhost
-iptables −A INPUT  −i lo −j ACCEPT
-iptables −A OUTPUT −o lo −j ACCEPT
+iptables -A INPUT  -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 
 ## A nuestras IP le dejamos todo
-iptables −A INPUT  −s $IP_UTEG_DES −j ACCEPT
-iptables −A OUTPUT −d $IP_UTEG_DES −j ACCEPT
-iptables −A INPUT  −s $IP_UTEG_OFI −j ACCEPT
-iptables −A OUTPUT −d $IP_UTEG_OFI −j ACCEPT
+iptables -A INPUT  -s $IP_UTEG_DES -j ACCEPT
+iptables -A OUTPUT -d $IP_UTEG_DES -j ACCEPT
+iptables -A INPUT  -s $IP_UTEG_OFI -j ACCEPT
+iptables -A OUTPUT -d $IP_UTEG_OFI -j ACCEPT
 
 ## El puerto 80 y 433 de www debe estar abierto, es un servidor web.
-iptables −A INPUT  −p tcp −−dport 80 −j ACCEPT
-iptables −A OUTPUT −p tcp −−sport 80 −j ACCEPT
-iptables −A INPUT  −p tcp −−dport 443 −j ACCEPT
-iptables −A OUTPUT −p tcp −−sport 443 −j ACCEPT
+iptables -A INPUT  -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 80 -j ACCEPT
+iptables -A INPUT  -p tcp --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 443 -j ACCEPT
 
 ## Cerramos otros puertos que estan abiertos
-iptables −A INPUT −p udp −−dport 5353 −j DROP
+iptables -A INPUT -p udp --dport 5353 -j DROP
 
 ## Se guarda la configuracion de Iptables
 iptables-save > /etc/sysconfig/iptables
 
-echo "OK. Verifique que lo que se aplica con: iptables −L −n"
+echo "OK. Verifique que lo que se aplica con: iptables -L -n"
 
 #proceso de bajar Firewalld e Instalar iptables
 #sudo yum install iptables-services
