31846375573a05de6d948ea6e6f062f0fcf0ac9b
gkviterig <analistadesarrollo01@uteg.edu.ec>
2019-06-12 13:19:09 -0500

    Actualizaciones en vistas de solicitudes por caso de une.

--- modules/admision/controllers/SolicitudesController.php
+++ modules/admision/controllers/SolicitudesController.php
@@ -1564,5 +1564,14 @@ class SolicitudesController extends \app\components\CController {
                     "emp_id" => $emp_id,
                     "arr_item" => ArrayHelper::map(array_merge(["id" => "0", "name" => "Seleccionar"], $resp_item), "id", "name"),
         ]);
-    }        
+    }
+    
+    public function actionSubirotrosdocumentos() {
+        $sol_id = base64_decode($_GET['id_sol']);
+        $sol_model = new SolicitudInscripcion();
+        $datosSolicitud = $sol_model->consultarInteresadoPorSol_id($sol_id);
+        return $this->render('subirOtrosDocumentos', [                    
+                    "datos" => $datosSolicitud,
+        ]);
+    }
 }
