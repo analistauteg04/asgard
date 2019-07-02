a941cbb216bf702e83db67042e1e9fd22b2f50e7
kleber Loayza <analistadesarrollo03.uteg.edu.ec>
2019-07-02 13:28:09 -0400

    avance pagos frecuentes

--- views/pagosfrecuentes/js/pagosfrecuentes.js
+++ views/pagosfrecuentes/js/pagosfrecuentes.js
@@ -339,7 +339,7 @@ function guardarFacturaTemp() {
 }
 
 function guardarPagos() {
-    /*var link = $('#txth_base').val() + "/pagosfrecuentes/savepayment";
+    var link = $('#txth_base').val() + "/pagosfrecuentes/savepayment";
     var arrParams = new Object();
     if (total == 0) {
         mensaje("No ha seleccionado productos para la factura.");
@@ -352,15 +352,14 @@ function guardarPagos() {
             requestHttpAjax(link, arrParams, function (response) {
                 showAlert("OK", "success", response.message);
                 sessionStorage.clear();
-                setTimeout(function () {*/
-                    //var bohre = $('#txth_base').val() + "/pagosfrecuentes/botonpago?docid=" + response.message.iddoc+"&popup=1";
-                    var bohre = $('#txth_base').val() + "/pagosfrecuentes/botonpago?docid=" + 3+"&popup=1";
+                setTimeout(function () {
+                    var bohre = $('#txth_base').val() + "/pagosfrecuentes/botonpago?docid=" + response.message.iddoc+"&popup=1";
                     $('#btn_pago_p').attr("href", bohre);
                     $('#btn_pago_p').trigger("click");                    
-        /*        }, 3000);
+                }, 3000);
             });
         }
-    }*/
+    }
 }
 
 function mensaje(lv_mensaje) {
