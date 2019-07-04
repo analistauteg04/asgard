7c36ed44f91c805d9f1b981c366e35c02cba24dc
GianVergara <analistadesarrollo02@uteg.edu.ec>
2019-07-04 16:42:24 -0500

    validacion de ruc, cedula y pasaporte, y campos obligatorios en datos factura pagos frecuentes

--- views/pagosfrecuentes/_form_pago.php
+++ views/pagosfrecuentes/_form_pago.php
@@ -19,7 +19,7 @@ aspirante::registerTranslations();
 ?>
 <form class="form-horizontal">
     <?= Html::hiddenInput('txth_ruc_lb', Yii::t("formulario", "RUC"), ['id' => 'txth_ruc_lb']); ?>
-    <?= Html::hiddenInput('txth_ced_lb', Yii::t("formulario", "DNI Document"). '/' . Yii::t("formulario", "DNI 1"), ['id' => 'txth_ced_lb']); ?>
+    <?= Html::hiddenInput('txth_ced_lb', Yii::t("formulario", "DNI Document") . '/' . Yii::t("formulario", "DNI 1"), ['id' => 'txth_ced_lb']); ?>
     <?= Html::hiddenInput('txth_pas_lb', Yii::t("formulario", "Passport"), ['id' => 'txth_pas_lb']); ?>
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <h3><span id="lbl_Personeria"><?= Yii::t("formulario", "Data Invoices") ?></span></h3>
@@ -33,7 +33,7 @@ aspirante::registerTranslations();
         <div class='col-md-12 col-sm-12 col-xs-12 col-lg-12'>
             <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
                 <div class="form-group">
-                    <label for="txt_nombres_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_nombre1"><?= Yii::t("formulario", "Names") ?></label>
+                    <label for="txt_nombres_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_nombre1"><?= Yii::t("formulario", "Names") ?><span class="text-danger">*</span></label>
                     <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
                         <input type="text" class="form-control PBvalidation keyupmce" value="" id="txt_nombres_fac" data-type="alfa" placeholder="<?= Yii::t("formulario", "Names") ?>">
                     </div>
@@ -41,7 +41,7 @@ aspirante::registerTranslations();
             </div> 
             <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
                 <div class="form-group">
-                    <label for="txt_apellidos_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_apellido1"><?= Yii::t("formulario", "Last Names") ?></label>
+                    <label for="txt_apellidos_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_apellido1"><?= Yii::t("formulario", "Last Names") ?><span class="text-danger">*</span></label>
                     <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
                         <input type="text" class="form-control PBvalidation keyupmce" value="" id="txt_apellidos_fac" data-type="alfa" placeholder="<?= Yii::t("formulario", "Last Names") ?>">
                     </div>
@@ -51,7 +51,7 @@ aspirante::registerTranslations();
         <div class='col-md-12 col-sm-12 col-xs-12 col-lg-12'>
             <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
                 <div class="form-group">
-                    <label for="txt_dir_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_nombre1"><?= Yii::t("formulario", "Address") ?></label>
+                    <label for="txt_dir_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_nombre1"><?= Yii::t("formulario", "Address") ?><span class="text-danger">*</span></label>
                     <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
                         <input type="text" class="form-control PBvalidation keyupmce" value="" id="txt_dir_fac" data-type="alfanumerico" placeholder="<?= Yii::t("formulario", "Address") ?>">
                     </div>
@@ -59,7 +59,7 @@ aspirante::registerTranslations();
             </div> 
             <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
                 <div class="form-group">
-                    <label for="txt_tel_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_apellido1"><?= Yii::t("formulario", "Phone") ?></label>
+                    <label for="txt_tel_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_apellido1"><?= Yii::t("formulario", "Phone") ?><span class="text-danger">*</span></label>
                     <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
                         <input type="text" class="form-control keyupmce" value="" id="txt_tel_fac" data-type="alfa" placeholder="<?= Yii::t("formulario", "Phone") ?>">
                     </div>
@@ -77,19 +77,43 @@ aspirante::registerTranslations();
                     </div>  
                 </div>
             </div>
-            <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
+            <!-- <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
                 <div class="form-group">
-                    <label for="txt_dni_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_dni"><?= Yii::t("formulario", "DNI Document") . '/' . Yii::t("formulario", "DNI 1") ?></label>
+                    <label for="txt_dni_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_dni"><? Yii::t("formulario", "DNI Document") . '/' . Yii::t("formulario", "DNI 1") ?></label>
                     <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
                         <input type="text" class="form-control PBvalidation keyupmce" value="" id="txt_dni_fac" data-type="number" data-lengthMax="10" data-lengthMin="10" placeholder="<?= Yii::t("formulario", "DNI Document") ?>">
                     </div>
                 </div>
-            </div>                 
+            </div>    -->  
+            <div class='col-md-6 col-sm-6 col-xs-6 col-lg-6' id="DivcedulaFac">  
+                <div class="form-group">
+                    <label for="txt_dni_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label" id="lbl_dni"><?= Yii::t("formulario", "DNI Document") . '/' . Yii::t("formulario", "DNI 1") ?><span class="text-danger">*</span></label>
+                    <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
+                        <input type="text" maxlength="10" class="form-control" value="" id="txt_dni_fac" data-type="cedula" data-keydown="true" placeholder="<?= Yii::t("formulario", "DNI Document") ?>">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6" style="display: none;" id="DivpasaporteFac">
+                <div class="form-group">
+                    <label for="txt_pasaporte_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label"><?= Yii::t("formulario", "Passport") ?> <span class="text-danger">*</span></label>
+                    <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
+                        <input type="text" maxlength="15" class="form-control keyupmce" id="txt_pasaporte_fac" data-type="alfanumerico" data-keydown="true" placeholder="<?= Yii::t("formulario", "Passport") ?>">
+                    </div>
+                </div>
+            </div>        
+            <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6" style="display: none;" id="DivRucFac">
+                <div class="form-group">
+                    <label for="txt_ruc_fac" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label"><?= Yii::t("formulario", "RUC") ?> <span class="text-danger">*</span></label>
+                    <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
+                        <input type="text" maxlength="13" class="form-control keyupmce" id="txt_ruc_fac" data-type="number" data-keydown="true" placeholder="<?= Yii::t("formulario", "RUC") ?>">
+                    </div>
+                </div>
+            </div>
         </div> 
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
                 <div class="form-group">
-                    <label for="txt_correo_factura" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label"><?= Yii::t("formulario", "Email") ?></label>
+                    <label for="txt_correo_factura" class="col-sm-5 col-md-5 col-xs-5 col-lg-5 control-label"><?= Yii::t("formulario", "Email") ?><span class="text-danger">*</span></label>
                     <div class="col-sm-7 col-md-7 col-xs-7 col-lg-7">
                         <input type="text" class="form-control PBvalidation" id="txt_correo_factura" data-type="email" data-keydown="true" placeholder="<?= Yii::t("formulario", "Email") ?>">
                     </div>
