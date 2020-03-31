1774d273b48f55953ed4ddc9bfc1bb9a482608f3
Grace Viteri <analistadesarrollo01@uteg.edu.ec>
2020-03-31 00:43:59 -0500

    Actualizaciones de script

---
+++ db/base_nueva_prod/data/script_para_produccion_matriculacion/db_asgard_data_2.sql
@@ -0,0 +1,12 @@
+update db_asgard.persona 
+set per_cedula = '100'
+where per_cedula = '0951984343';
+
+update db_asgard.persona 
+set per_cedula = '101'
+where per_cedula = '0923505614';
+
+update db_asgard.persona 
+set per_cedula = '102'
+where per_cedula = '0926976572';
+
