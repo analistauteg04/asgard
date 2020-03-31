7477e0382afb63e981486903bb3cecb3c9632ed5
Grace Viteri <analistadesarrollo01@uteg.edu.ec>
2020-03-31 01:46:26 -0500

    Scripts - producción

--- db/base_nueva_prod/data/script_para_produccion_matriculacion/db_asgard_data_2.sql
+++ db/base_nueva_prod/data/script_para_produccion_matriculacion/db_asgard_data_2.sql
@@ -10,3 +10,7 @@ update db_asgard.persona
 set per_cedula = '102'
 where per_cedula = '0926976572';
 
+update db_asgard.grup_rol
+set grol_estado = '0'
+where grol_id = 32;
+
--- db/base_nueva_prod/data/script_para_produccion_matriculacion/db_asgard_estructura.sql
+++
@@ -1,1 +0,0 @@
-alter table db_asgard.usuario add column usu_upreg varchar(1) null after usu_link_activo;
