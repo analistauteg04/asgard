df34563ba7aab93bfaf24c9c68809c5f956428b5
GianVergara <analistadesarrollo02@uteg.edu.ec>
2020-06-16 12:20:14 -0500

    vista subir pago en modulo pagos

--- modules/financiero/controllers/PagosfacturasController.php
+++ modules/financiero/controllers/PagosfacturasController.php
@@ -12,6 +12,9 @@ use app\models\Usuario;
 use yii\helpers\Url;
 use yii\base\Exception;
 use yii\base\Security;
+use app\modules\academico\models\Especies;
+use app\modules\financiero\models\FormaPago;
+use app\modules\admision\models\Oportunidad;
 use app\modules\academico\models\Modalidad;
 use app\modules\academico\models\UnidadAcademica;
 use app\modules\academico\models\ModuloEstudio;
@@ -93,6 +96,31 @@ class PagosfacturasController extends \app\components\CController {
                     'arr_unidad' => ArrayHelper::map($arr_unidadac, "id", "name"),
                     'arr_modalidad' => ArrayHelper::map($arr_modalidad, "id", "name"),
                     'arr_carrera' => ArrayHelper::map($carrera, "id", "name"),
+        ]);
+    }
+    public function actionSubirpago() {
+        $per_idsession = @Yii::$app->session->get("PB_perid");
+        $especiesADO = new Especies();
+        $mod_unidad = new UnidadAcademica();
+        $mod_modalidad = new Modalidad();
+        $modestudio = new ModuloEstudio();
+        $modcanal = new Oportunidad();
+        $mod_fpago = new FormaPago();
+        $personaData = $especiesADO->consultaDatosEstudiante($per_idsession);
+        $arr_unidadac = $mod_unidad->consultarUnidadAcademicas();
+        $arr_modalidad = $mod_modalidad->consultarModalidad($personaData['uaca_id'], 1);
+        if (($personaData['uaca_id'] == 1) or ( $personaData['uaca_id'] == 2)) {
+            $carrera = $modcanal->consultarCarreraModalidad($personaData['uaca_id'], $personaData['mod_id']);
+        } else {
+            $carrera = $modestudio->consultarCursoModalidad($personaData['uaca_id'], $personaData['mod_id']); // tomar id de impresa
+        }
+        $arr_forma_pago = $mod_fpago->consultarFormaPago();
+        return $this->render('subirpago', [
+                    'arr_persona' => $personaData,
+                    'arr_unidad' => ArrayHelper::map($arr_unidadac, "id", "name"),
+                    'arr_modalidad' => ArrayHelper::map($arr_modalidad, "id", "name"),
+                    'arr_carrera' => ArrayHelper::map($carrera, "id", "name"),
+                    "arr_forma_pago" => ArrayHelper::map($arr_forma_pago, "id", "value"),
         ]);
     }
 }
