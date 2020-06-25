4cb018a94c481545508bf9936926cb6b329125d5
Grace Viteri <analistadesarrollo01@uteg.edu.ec>
2020-06-25 11:48:32 -0500

    Actualizaciones varias- Merge

--- db/base_nueva_prod/estructura/db_academico.sql
+++ db/base_nueva_prod/estructura/db_academico.sql
@@ -1664,7 +1664,7 @@ CREATE TABLE `certificados_generadas` (
   `cgen_fecha_caducidad` date DEFAULT NULL,
   `cgen_ruta_archivo_pdf` varchar(500) DEFAULT NULL,
   `cgen_estado_certificado` varchar(1) NOT NULL, -- null pendiente, 1 codigo generado, 2 certificado generado 
-  `cgen_observacion_autorizacion` varchar(500) NOT NULL,
+  `cgen_observacion_autorizacion` varchar(500) NULL,
   `cgen_fecha_autorizacion` timestamp null DEFAULT NULL,
   `cgen_usuario_autorizacion` bigint(20) DEFAULT NULL,
   `cgen_usuario_ingreso` bigint(20) DEFAULT NULL,
