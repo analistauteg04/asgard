9f54744d19c71964baf10e40c8d15efde456b861
GianVergara <analistadesarrollo02@uteg.edu.ec>
2020-12-16 19:51:57 -0500

    cambios varios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -211,8 +211,8 @@ class UsuarioController extends CController
     public function actionGeneraclaves() {
         $usuario = new Usuario();     
         $security = new Security();
-        $dataInicial = 48;
-        $dataFinal = 50;
+        $dataInicial = 1480;
+        $dataFinal = 1483;
         $resul = $usuario->consultarDataUsuario($dataInicial, $dataFinal);
         if (count($resul)>0) {            
             for ($i=0; $i<count($resul); $i++) {                
